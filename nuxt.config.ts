c2aee84f8d031e9ab908247f28016433e72c1611
Hesan Abbasi <h.abbasi@avijeh.local>
2026-05-18 17:55:53 +0330

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  
+  // خودت این بخش modules را اضافه کن
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ]
+})
