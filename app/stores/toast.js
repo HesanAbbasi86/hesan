ee4cc852380cc9b97b3af2248ac60ac2316ef035
HesanAbbasi86 <hesanabbasi155@gmail.com>
2026-05-19 17:40:48 +0330

    Update toast.js

--- app/stores/toast.js
+++ app/stores/toast.js
@@ -9,15 +9,14 @@ export const useToastStore = defineStore('toast', {
     message: 'عملیات با موفقیت انجام شد! 🎉',
     type: 'success',
     position: 'top-right',
-    // پس‌زمینه تیره + بردر سبز + متن سبز + هاور شفاف سبز
+    
     className: 'bg-slate-800/80 border-2 border-green-500 text-green-400 hover:bg-green-500/10'
   },
   {
     label: 'Error ',
     message: 'خطایی در اتصال به سرور رخ داد! ❌',
     type: 'error',
-    position: 'top-left',
-    // پس‌زمینه تیره + بردر قرمز + متن قرمز + هاور شفاف قرمز
+    position: 'top-left'
     className: 'bg-slate-800/80 border-2 border-red-500 text-red-400 hover:bg-red-500/10'
   },
   {
@@ -25,7 +24,6 @@ export const useToastStore = defineStore('toast', {
     message: 'لطفاً اطلاعات خود را بازبینی کنید. ⚠️',
     type: 'warning',
     position: 'bottom-right',
-    // پس‌زمینه تیره + بردر زرد + متن زرد + هاور شفاف زرد
     className: 'bg-slate-800/80 border-2 border-yellow-500 text-yellow-400 hover:bg-yellow-500/10'
   },
   {
@@ -33,7 +31,6 @@ export const useToastStore = defineStore('toast', {
     message: 'این یک پیام وضعیت معمولی است. ℹ️',
     type: 'info',
     position: 'bottom-left',
-    // پس‌زمینه تیره + بردر آبی + متن آبی + هاور شفاف آبی
     className: 'bg-slate-800/80 border-2 border-blue-500 text-blue-400 hover:bg-blue-500/10'
   }
 ]
